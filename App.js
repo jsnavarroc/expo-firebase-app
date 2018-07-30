a50228e48e814fc68b91939179d32f47e755ef1d
Johan Navarro <21sebas12@gmail.com>
2018-07-29 23:03:29 -0500

    Se creo un componente que carga el background de la aplicacion

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppButton from './application/components/AppButton';
 import PreLoader from './application/components/PreLoader';
+import BackgroundImage from './application/components/BackgroundImage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PreLoader/>
+     <BackgroundImage
+      source = {require('./assets/images/3.jpg') }
+     >
+      <Text style = {{color:'#fff', marginTop:100 }}>Hola</Text>
+     </BackgroundImage>
     );
   }
 }
