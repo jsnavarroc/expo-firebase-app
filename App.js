65de5243bc8b17e3e0c3809c6c62be48d7563be1
Johan Navarro <21sebas12@gmail.com>
2018-08-05 16:52:38 -0500

    Establecemos la logiaca con firebase para saber cuando un usuario se logeo correctamente

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import GuestNavigation from './application/routes/guest';
+import firebase from 'firebase'; 
+import {Text} from 'react-native-elements';
+import PreLoader from './application/components/PreLoader';
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLogged : false,
+      loaded : false,
+    }
+  }
+  
+  async componentWillMount(){
+    await firebase.auth().onAuthStateChanged(user => {
+      if(user!==null){
+        this.setState({
+          isLogged : false,
+          loaded : true,
+        });
+      }
+    });
+  }
+  
   render() {
-    return (
-      <GuestNavigation/>
-    );
+    const {isLogged, loaded} = this.state;
+    if(!loaded){
+      return (<PreLoader/>)
+    }
+    if(isLogged){
+      return (<Text> Logueado </Text>);
+    }else{
+      return (<GuestNavigation/>); 
+    }
   }
 }
 
